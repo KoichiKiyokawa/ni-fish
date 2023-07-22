ce3fbd6f3cb1050000f3923c9d79858f8d504615
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-07-22 19:18:53 +0900

    fix: file colocation

--- functions/nr.fish
+++ functions/nr.fish
@@ -1,16 +1,16 @@
-function ni -d "Install packages with correct package manager"
+function nr -d "Run script with correct package manager"
   if test -f package-lock.json
     echo -e "[ni-fish] Use npm\n"
-    npm install $argv
+    npm run $argv
   else if test -f yarn.lock
     echo -e "[ni-fish] Use yarn\n"
-    yarn $argv
+    yarn run $argv
   else if test -f pnpm-lock.yaml
     echo -e "[ni-fish] Use pnpm\n"
-    pnpm install $argv
+    pnpm run $argv
   else if test -f bun.lockb
     echo -e "[ni-fish] Use bun\n"
-    bun install $argv
+    bun run $argv
   else
     echo -e "No lock file found. Please run npm install, yarn install, or pnpm install.\n"
   end
