cdb76c53b4f9495e75a30e828e04d689da3a009d
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-07-22 18:57:14 +0900

    fix: completion

--- completions/ni.fish
+++ completions/ni.fish
@@ -1,7 +1,8 @@
-function __fish_complete_nr
-  if test -f ./package.json
-    node -pe "try{Object.keys(require('./package.json').scripts).join(' ')}catch{'No package.json file found.\n'}"
+if test -f ./package.json
+  set cmds (node -pe "try{Object.entries(require('./package.json').scripts).map(([k, v]) => k+','+v).join('\n')}catch{'No package.json file found.\n'}")
+  for cmd in $cmds
+    set -l name (echo $cmd | awk -F',' '{ print $1 }')
+    set -l command (echo $cmd | awk -F',' '{ $1=""; print $0 }' | string trim)
+    complete -c nr -f -a $name -d $command
   end
 end
-
-complete -c nr -f -a '(__fish_complete_nr)'
