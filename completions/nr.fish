bf0f56616049b73da447cc7ee1086a36cd35002e
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-07-23 22:25:39 +0900

    refactor: rename variable

--- completions/nr.fish
+++ completions/nr.fish
@@ -1,8 +1,8 @@
 if test -f ./package.json
-  set cmds (node -pe "try{Object.entries(require('./package.json').scripts).map(([k, v]) => k+','+v).join('\n')}catch{'No package.json file found.\n'}")
-  for cmd in $cmds
-    set -l name (echo $cmd | awk -F',' '{ print $1 }')
-    set -l command (echo $cmd | awk -F',' '{ $1=""; print $0 }' | string trim)
+  set scripts (node -pe "try{Object.entries(require('./package.json').scripts).map(([k, v]) => k+','+v).join('\n')}catch{'No package.json file found.\n'}")
+  for script in $scripts
+    set -l name (echo $script | awk -F',' '{ print $1 }')
+    set -l command (echo $script | awk -F',' '{ $1=""; print $0 }' | string trim)
     complete -c nr -f -a $name -d $command
   end
 end
