e6e8fab165a65188bd2187b180353cb21dd7da34
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-07-23 22:25:30 +0900

    feat: show running command

--- functions/ni.fish
+++ functions/ni.fish
@@ -1,17 +1,18 @@
 function ni -d "Install packages with correct package manager"
   if test -f package-lock.json
-    echo -e "[ni-fish] Use npm\n"
+    echo -e "[ni-fish] npm install $argv\n"
     npm install $argv
   else if test -f yarn.lock
-    echo -e "[ni-fish] Use yarn\n"
-    yarn $argv
+    echo -e "[ni-fish] yarn install $argv\n"
+    yarn install $argv
   else if test -f pnpm-lock.yaml
-    echo -e "[ni-fish] Use pnpm\n"
+    echo -e "[ni-fish] pnpm install $argv\n"
     pnpm install $argv
   else if test -f bun.lockb
-    echo -e "[ni-fish] Use bun\n"
+    echo -e "[ni-fish] bun install $argv\n"
     bun install $argv
   else
     echo -e "No lock file found. Please run npm install, yarn install, or pnpm install.\n"
   end
 end
+
--- functions/nr.fish
+++ functions/nr.fish
@@ -1,17 +1,18 @@
 function nr -d "Run script with correct package manager"
   if test -f package-lock.json
-    echo -e "[ni-fish] Use npm\n"
+    echo -e "[ni-fish] npm run $argv\n"
     npm run $argv
   else if test -f yarn.lock
-    echo -e "[ni-fish] Use yarn\n"
+    echo -e "[ni-fish] yarn run $argv\n"
     yarn run $argv
   else if test -f pnpm-lock.yaml
-    echo -e "[ni-fish] Use pnpm\n"
+    echo -e "[ni-fish] pnpm run $argv\n"
     pnpm run $argv
   else if test -f bun.lockb
-    echo -e "[ni-fish] Use bun\n"
+    echo -e "[ni-fish] bun run $argv\n"
     bun run $argv
   else
     echo -e "No lock file found. Please run npm install, yarn install, or pnpm install.\n"
   end
 end
+
